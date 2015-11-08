7ac7fac0f0bd749c0c00132942178c21331716ae
kako <claudio.melendrez@gmail.com>
2015-11-08 20:54:45 -0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # django-signals-mixin
-Use Django signals (almost) like JavaScript events
+
+Use Django signals (almost) like JavaScript events.
+
+## Why?
+
+Because I like JavaScript's event triggering and listening, so I thought, "Why
+not tune Django signals to use them in a same way?"
+
+## How do I use them?
+
+Simply add the `SignalsMixin` to your model's base classes, and then attach
+receivers using the class' `on_signal` method, like so:
+
+```
+from django.db import models
+from django_signals_mixin import SignalsMixin
+
+class MyModel(models.Model, SignalsMixin):
+    ...
+
+MyModel.on_signal('saved', alert_mymodel_saved)
+```
+
+## Wait, you attached the receiver to the class!
+
+Yes, because in Python (unlike JavaScript) we have classes, so we can! So we
+attach signal listeners only once (to the class), instead of attaching it to
+every single instance.
+
+## What data do receivers get?
+
+All signals send to receivers the following parameters:
+
+* sender: model of the instance that sent the signal
+* instance: instance that sent the signal
+
+Default signals also send some extra data, depending on the signal (check the Django
+documentation). Custom signals also send a *name* parameter, which is their key
+in the dictionary that defines them.
+
+## Did you say custom signals?
+
+Yes, if you need custom signals simply override the `CUSTOM_SIGNALS` attribute
+in the model (which is empty by default) and then signal it:
+
+```
+...
+
+class MyModel(models.Model, SignalsMixin):
+
+    CUSTOM_SIGNALS = {
+        'played': Signal()
+    }
+
+    def play(self):
+        ...
+        self.signal('played)'
+```
+
+That's it. Now all receivers that have registered to that signal will execute
+when it's sent.
---
+++ django_signals_mixin.py
@@ -0,0 +1,65 @@
+__author__ = 'kako'
+
+from django.db.models.signals import post_save, post_delete
+
+
+class SignalsMixin(object):
+    """
+    Provide generic, simplified signal connection and sending mechanisms to
+    Django models, somewhat inspired by JavaScript events.
+
+    By default adds 'saved' and 'deleted' signals ready to use, and more can
+    be added by overriding the CUSTOM_SIGNALS dictionary. All sent signals
+    pass the model instance as the 'instance' parameter, and custom ones
+    also pass a 'name', which is the key in CUSTOM_SIGNALS.
+
+    Example use:
+        class SomeModel(models.Model, SignalsMixin):
+            CUSTOM_SIGNALS = {'viewed': Signal()}
+            ...
+            def mark_as_viewed(self):
+                self.status = 'viewed'
+                self.save()
+                self.signal('viewed')
+
+        SomeModel.on_signal('saved', do_something)
+        SomeModel.on_signal('viewed', do_something_else)
+    """
+    _DEFAULT_SIGNALS = {
+        'saved': post_save,
+        'deleted': post_delete
+    }
+    CUSTOM_SIGNALS = {}
+
+    @classmethod
+    def _get_signal(cls, name):
+        """
+        Get the signal with the given name in defaults or custom signals.
+        """
+        signal = cls._DEFAULT_SIGNALS.get(name)
+        if not signal:
+            signal = cls.CUSTOM_SIGNALS.get(name)
+
+        if not signal:
+            raise TypeError("No signal named '{}' found for {}.".format(name, cls))
+
+        return signal
+
+    @classmethod
+    def on_signal(cls, name, receiver):
+        """
+        Connect the given receiver to the signal of the given type, raising
+        an error if the type doesn't exist.
+        """
+        signal = cls._get_signal(name)
+        uid = '{}.{}'.format(cls._meta.model_name, name)
+        signal.connect(receiver, sender=cls, dispatch_uid=uid)
+
+    def signal(self, name, **kwargs):
+        """
+        Send the signal of the given type if, raising an error if the type
+        doesn't exist.
+        """
+        signal = self._get_signal(name)
+        signal.send(sender=self.__class__, instance=self, name=name, **kwargs)
+
